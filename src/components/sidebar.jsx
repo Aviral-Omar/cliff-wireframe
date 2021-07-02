b395cf78056e461c1dfcf7315baa1f6d1d1eafff
Aviral-Omar <f20190411@pilani.bits-pilani.ac.in>
2021-07-02 23:19:46 +0530

    Added icons and improved collapsed sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Layout, Menu, Avatar, Row, Col, Typography, Button } from 'antd';
+import { HomeOutlined, UnorderedListOutlined, AreaChartOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 
 const { Sider } = Layout;
 
 const Sidebar = props => {
 	const [name, setName] = useState('');
+	const [collapsed, setCollapsed] = useState(true);
 
 	const signOut = () => props.removeToken();
 
@@ -27,31 +29,37 @@ const Sidebar = props => {
 
 	return (
 		<Sider
-			width={240}
-			breakpoint="md"
-			collapsedWidth={0}
+			width={200}
+			collapsedWidth={80}
 			collapsible
+			defaultCollapsed={collapsed}
+			onCollapse={() => setCollapsed(!collapsed)}
 			style={{ backgroundColor: 'white' }}
 		>
 			<Row justify="space-around" align="middle" style={{ margin: '16px 0px' }}>
 				<Avatar shape="square" size={64}>
 					USER
 				</Avatar>
-				<Col>
-					<Typography.Title level={4} style={{ marginBottom: '0px' }}>
-						{name}
-					</Typography.Title>
-					<Button type="text" onClick={signOut}>
-						Sign Out
-					</Button>
-				</Col>
+				{collapsed ? null : (
+					<Col>
+						<Typography.Title level={4} style={{ marginBottom: '0px' }}>
+							{name}
+						</Typography.Title>
+						<Button type="text" onClick={signOut}>
+							Sign Out
+						</Button>
+					</Col>
+				)}
 			</Row>
 			<Menu>
-				<Menu.Item key={1}>
+				<Menu.Item key={1} icon={<HomeOutlined />}>
 					<Link to="/home">Home</Link>
 				</Menu.Item>
-				<Menu.Item key={2}>
+				<Menu.Item key={2} icon={<UnorderedListOutlined />}>
 					<Link to="/metrics">Metrics</Link>
+				</Menu.Item>
+				<Menu.Item key={3} icon={<AreaChartOutlined />}>
+					<Link to="/streams">Streams</Link>
 				</Menu.Item>
 			</Menu>
 		</Sider>
