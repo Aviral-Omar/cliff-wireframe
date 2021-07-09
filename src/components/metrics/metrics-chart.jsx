76a30619aac16b3a530ac616dfb2aced2b1b520a
Anchit Shukla <anchit.shukla@greendeck.co>
2021-07-09 17:58:16 +0530

    Modify metric chart

--- src/components/metrics/metrics-chart.jsx
+++ src/components/metrics/metrics-chart.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, Typography } from 'antd';
-import ReactEChartsCore from 'echarts-for-react/lib/core';
+import ReactEChartsCore from 'echarts-for-react';
 import * as echarts from 'echarts/core';
 import { LineChart } from 'echarts/charts';
 import { TitleComponent, TooltipComponent, GridComponent } from 'echarts/components';
@@ -13,7 +13,7 @@ echarts.use([TitleComponent, TooltipComponent, GridComponent, LineChart, CanvasR
 const Chart = props => {
 	const { metric, tsData } = props;
 
-	const formattedData = tsData?.rows.map(row => [row.timestamp, row.original_value]);
+	const formattedData = tsData?.rows.map(row => [new Date(row.timestamp), row.original_value]);
 	return (
 		<Card
 			title={metric._source.measure}
@@ -25,24 +25,22 @@ const Chart = props => {
 			))}
 		>
 			<ReactEChartsCore
-				echarts={echarts}
 				option={{
 					toottip: {
 						trigger: 'axis',
 					},
 					xAxis: {
 						type: 'time',
+						data: formattedData.map(dataPoint => dataPoint[0])
 					},
 					yAxis: {
 						type: 'value',
 					},
-					source: {
-						data: formattedData,
+					series: [{
 						type: 'line',
-					},
+						data: formattedData.map(dataPoint => dataPoint[1])
+					}]
 				}}
-				notMerge
-				lazyUpdate
 			/>
 		</Card>
 	);
